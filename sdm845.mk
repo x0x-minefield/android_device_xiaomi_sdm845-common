7248a5eb444682dce746f0bf73da65edff607e22
LuK1337 <priv.luk@gmail.com>
2021-04-23 20:15:40 +0200

    sdm845-common: Remove fs_config_files from PRODUCT_PACKAGES
    Not needed anymore.
    
    Change-Id: Iaa474730fe5fdb5d5e159af127613fa4a22fc99e

--- sdm845.mk
+++ sdm845.mk
@@ -56,10 +56,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
-# AID/fs configs
-PRODUCT_PACKAGES += \
-    fs_config_files
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
